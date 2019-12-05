c04b4008369909b03fffbfd94fcaf950e2657748
Krzysztof Geleta <kgeleta@student.agh.edu.pl>
2019-12-05 18:21:07 +0100

    fixed bad merge

--- app/src/main/java/com/vastmoths/beerapp/ui/listBeers/ListBeerJavaFragment.java
+++ app/src/main/java/com/vastmoths/beerapp/ui/listBeers/ListBeerJavaFragment.java
@@ -13,48 +13,36 @@ import androidx.fragment.app.Fragment;
 import com.vastmoths.beerapp.R;
 import com.vastmoths.beerapp.database.DatabaseCRUD;
 import com.vastmoths.beerapp.database.model.Beer;
+import com.vastmoths.beerapp.ui.detailBeer.DetailBeerJavaFragment;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ListBeerJavaFragment extends Fragment {
+public class ListBeerJavaFragment extends Fragment implements AdapterView.OnItemClickListener {
 
     private DatabaseCRUD databaseCRUD;
+
+    private List<Beer> beers;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        final View rootView = inflater.inflate(R.layout.fragment_list_beer, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_list_beer, container, false);
         this.databaseCRUD  = new DatabaseCRUD(rootView.getContext());
 
-        databaseCRUD.insertBeer(new Beer(0, "tyskie", 2, "jasne", "pic"));
+//        databaseCRUD.insertBeer(new Beer(0, "tyskie", 2, "jasne", "pic"));
 
         try
         {
             final ListView listView = (ListView)rootView.findViewById(R.id.beerList);
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                    // this is called after user click on item in list
-                    System.out.println("you clicked " + listView.getItemAtPosition(position));
-
-                    String str = "you clicked " + listView.getItemAtPosition(position) + " " + position;
-
-//                    MainActivity.Companion.debug(str);
-
-//                    rootView.setVisibility(View.INVISIBLE);
-//                    DetailBeerFragment fragment = new DetailBeerFragment();
-//                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-//                    fragmentManager.beginTransaction()
-//                            .replace(R.id.nav_host_fragment, fragment)
-//                            .commit();
-                }
-            });
+            listView.setOnItemClickListener(this);
+            beers = databaseCRUD.getGetAllBeers();
 
             ArrayList beerNames = new ArrayList<String>();
             for (Beer beer :
-                    databaseCRUD.getGetAllBeers()) {
+                    beers) {
+                System.out.println(beer.getType());
                 beerNames.add(beer.getName());
             }
             ArrayAdapter arrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, beerNames);
@@ -69,4 +57,14 @@ public class ListBeerJavaFragment extends Fragment {
         return rootView;
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//        System.out.println("you clicked " + listView.getItemAtPosition(position));
+
+        DetailBeerJavaFragment nextFrag= new DetailBeerJavaFragment(beers.get(position));
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(this.getId(), nextFrag, "findThisFragment")
+                .addToBackStack(null)
+                .commit();
+    }
 }
